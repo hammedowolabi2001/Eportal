cf459fba3c62ed9b4f7e190ea5731ab6918d0581
Hammed <owolabihammed2001@gmail.com>
2020-09-04 18:04:10 -0700

    new connection

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$host= 'localhost';
-$username='root';
-$password='';
-$db= 'modrosa';
+$host= 'z8dl7f9kwf2g82re.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username='cvf2t27kpj5m65sb';
+$password='	nnnfpk5zfzk7zmdl';
+$db= 'zvprmsw42f8y7jh2';
 
 $connect= mysqli_connect($host, $username, $password,$db);
 // if($connect){
